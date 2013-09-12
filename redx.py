4be8f2b371af46650092bb8a93cef04cc4828477
jurlak <jurlak@jurlak-Notebook.(none)>
2013-09-12 09:53:59 +0300

    redx.py

---
+++ redx.py
@@ -0,0 +1,30 @@
+print 'spam eggs'
+
+print 'doesn\'t'
+
+print "doesn't"
+
+print '"Yes," he said.'
+
+print "Yes, \n he said."
+
+print '"Isn,t" she said.'
+
+hello=  "This is a rather long string containing\n\
+several lines of text just as you would do in C.\n\
+    Note that whitespace at the beginning of the line is\
+ significant."
+
+print hello
+
+print """
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+"""
+
+hello = r"This is a rather long string containing\n\
+several lines of text much as you would do in C."
+
+print hello
+
