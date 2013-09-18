2bc17678b161d7514640b3c78eaa026625369d89
jurlak <jurlak@jurlak-Notebook.(none)>
2013-09-18 18:21:51 +0300

    kodus.py

---
+++ kodus.py
@@ -0,0 +1,13 @@
+print '{0:*^60}'.format('Teretulemast')  # use '*' as a fill char
+
+
+print#
+
+from time import gmtime, strftime
+print strftime("%a, %d %b %Y %H:%M:%S GSM +0 Time", gmtime())
+
+print#
+
+
+
+
