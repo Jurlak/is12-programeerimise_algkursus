7cac22438b168de4f05458886bd473c775944693
jurlak <jurlak@jurlak-Notebook.(none)>
2013-09-18 17:38:43 +0300

    kodune.py

--- kodune.py
+++ kodune.py
@@ -10,11 +10,6 @@ c = 3-5j
 print ('The complex number {0} is formed from the real part {0.real} '
 'and the imaginary part {0.imag}.').format(c)
 
-
-
-print "Kaal:{0} {1} ".format(9+2/14*2,"kg")
-
-
 class Point(object):
      def __init__(self, x, y):
          self.x, self.y = x, y
@@ -23,8 +18,37 @@ class Point(object):
 
 print str(Point(4, 2))
 
-kaal= 5*7/2+2*3
-print "Kaal:{0} {1}".format(kaal,"kg")
+coord = (3, 5)
+print'X: {0[0]};  Y: {0[1]}'.format(coord)
+
+print "repr() shows quotes: {0!r}; str() doesn't: {1!s}".format('test1', 'test2')
+
+print '{0:<30}'.format('left aligned')
+
+print '{0:>30}'.format('right aligned')
+
+print '{0:^30}'.format('centered')
+
+print '{0:*^30}'.format('centered')  # use '*' as a fill char
+
+print '{0:+f}; {0:+f}'.format(3.14, -3.14)  # show it always
+
+print '{0: f}; {0: f}'.format(3.14, -3.14)  # show a space for positive numbers
+
+print '{0:-f}; {0:-f}'.format(3.14, -3.14)  # show only the minus -- same as '{0:f}; {0:f}'
+
+print "int: {0:d};  hex: {0:x};  oct: {0:o};  bin: {0:b}".format(42)
+
+print "int: {0:d};  hex: {0:#x};  oct: {0:#o};  bin: {0:#b}".format(42)
+
+
+points = 19.5
+total = 22
+print 'Correct answers: {0:.2%}.'.format(points/total)
+
+import datetime
+d = datetime.datetime(2010, 7, 4, 12, 15, 58)
+print'{0:%Y-%m-%d %H:%M:%S}'.format(d)
 
 
 
