8a7eb6f76f3f5e004aa2b1a14087667c468903ff
jurlak <jurlak@jurlak-Notebook.(none)>
2013-09-25 11:42:33 +0300

    tunnitöö 25.09

---
+++ 25tund.py
@@ -0,0 +1,18 @@
+from random import randint
+klass=['egert','timo','arti','aigar','alex','mariin']
+print len(klass)
+
+
+n = randint(0, 5)
+element=klass[n]
+print klass [n]
+
+del klass [n]
+print klass
+
+klass=klass+[element]
+print klass
+
+print #
+
+
