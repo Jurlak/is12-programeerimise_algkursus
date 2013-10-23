33f0e5653ce72c399adfe72929244df65046631d
Jurlak <jasper.kirotosk@gmail.com>
2013-10-23 10:54:30 +0300

    parandus 2 tsyklid

--- kodus220913.py
+++ kodus220913.py
@@ -1,8 +1,10 @@
-x=0
-while x<100:
-	print "{0:02}".format(x),
-	x=x+1
-	print "",
-while x%10==9:
-	print "{0:02}".format(x)+ "\n"
-	x=x+1
+y = 0
+while y<10:
+	
+	x = 0
+	while x < 10:
+		print "{0:02}".format(10*y+x) ,
+		x=x + 1
+	print ""
+	
+	y = y + 1
