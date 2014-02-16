5840b72f245a923867bd6f92172b74ebc80a4031
Jasper <jasper991@hotmail.com>
2014-02-16 17:33:26 +0200

    poolik

---
+++ deitabeiss.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+
+
+print "add- lisab andmebaasi masiivi"
+print "rm- eemaldab masiivist teatud rea"
+print "ls- kuvatakse andmebaas"
+print "q- lahkuge andmebaasist"
+print " "
+kys= raw_input("valige endale sobiv varjant: ")
+
+
+cardb= [
+
+
+['VW', 'Phaeton', 'diisel'],
+['BMW', '750', 'bensiin'],
+['Volvo', 'S80', 'diisel'],
+['Audi', 'A7', 'diisel'],
+['Mercedes-Benz', 'SL 500', 'diisel'],
+]
+
+x=len(cardb)
+
+
+def add():
+	if kys=='add':
+		cardb.insert(x,[raw_input("Sisestage automark: "), raw_input("Sisestage auto mudel: "), raw_input("Sisestage bensiiniliik: ")]) 
+
+add()
+
+def rm():
+	if kys=='rm':
+		print cardb
+		cardb.pop(int(raw_input("sisestage number 0-..., et mõni automark eemaldada: ")))
+rm()
+
+def ls():
+	if kys=='ls':
+		print cardb
+ls()		
+
+def quit():
+	if kys=='q':
+		sys.exit(0)
+quit()
